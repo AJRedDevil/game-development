512ff15eaa44f7c2d6321a85a3717ab0c4533310
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-21 10:21:08 +0545

    feat(drawing): Drawing a basic Ship
    Add function to draw a basic ship
    Add options for a guide around the ship as well
    
    Closes #23

---
+++ exercise4/drawing.js
@@ -0,0 +1,87 @@
+function draw_grid({
+  ctx,
+  major,
+  minor = 10,
+  stroke = '#00FF00',
+  fill = '#009900',
+}) {
+  const finalMajor = major || minor * 5;
+  ctx.save();
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  let width = ctx.canvas.width;
+  let height = ctx.canvas.height;
+
+  for (let x = 0; x < width; x += minor) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.lineWidth = x % finalMajor == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (x % finalMajor == 0) {
+      ctx.fillText(x, x, 10);
+    }
+  }
+  for (let y = 0; y < height; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+    ctx.lineWidth = y % finalMajor == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (y % finalMajor == 0) {
+      ctx.fillText(y, 0, y + 10);
+    }
+  }
+  // These save and restore canvas state such as
+  // context.lineWidth, context.strokeStyle, and context.fillStyle.
+  // Using them in this way ensures that the canvas is restored to
+  // its original state after the function is done.
+  // This is good practice when writing drawing code.
+  // It avoids side effects and allows the calling code to keep control
+  // of the canvas context.
+  ctx.restore();
+}
+
+function draw_ship({ctx, x, y, radius, options = {}}) {
+  ctx.save();
+  // optionally draw a guide showing the collision radius
+  if (options.guide) {
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  // set some default values;
+  ctx.lineWidth = options.lineWidth || 2;
+  ctx.strokeStyle = options.strokeStyle || 'white';
+  ctx.fillStyle = options.fillStyle || 'black';
+  let angle = (options.angle || 0.5 * Math.PI) / 2;
+  // draw the ship in three lines
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  // Basically all you will need to know for now is that the sine and
+  // cosine functions allow the (x, y) coordinates to be calculated from
+  // a distance(a radius) and a direction(an angle).
+  // In JavaScript, there are two corresponding methods of the Math object:
+  // Math.sin(θ) and Math.cos(θ).
+  // With these, we can calculate the x - and y - coordinates relative to
+  // the center of the circle for any given angle, θ.
+  // To convert an angle and a distance to x - and y - coordinates,
+  // simply multiply these values by the distance you need(the radius of the circle).
+  ctx.lineTo(
+    x + Math.cos(Math.PI - angle) * radius,
+    y + Math.sin(Math.PI - angle) * radius
+  );
+  ctx.lineTo(
+    x + Math.cos(Math.PI + angle) * radius,
+    y + Math.sin(Math.PI + angle) * radius
+  );
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
