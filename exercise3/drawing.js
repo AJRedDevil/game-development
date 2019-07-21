88648b10d9793935a32dbb4bda3a2af781d0568e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-21 09:00:19 +0545

    feat(drawing): A Simple Pac-man
    Pac-Man is just a circle with a wedge cut out
    Used canvas API arc
    
    Closes #19

---
+++ exercise3/drawing.js
@@ -0,0 +1,43 @@
+function draw_grid({
+  ctx,
+  major,
+  minor = 10,
+  stroke = '#00FF00',
+  fill = '#009900',
+}) {
+  const finalMajor = major || minor * 5;
+  ctx.save();
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  let width = ctx.canvas.width;
+  let height = ctx.canvas.height;
+
+  for (let x = 0; x < width; x += minor) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.lineWidth = x % finalMajor == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (x % finalMajor == 0) {
+      ctx.fillText(x, x, 10);
+    }
+  }
+  for (let y = 0; y < height; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+    ctx.lineWidth = y % finalMajor == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (y % finalMajor == 0) {
+      ctx.fillText(y, 0, y + 10);
+    }
+  }
+  // These save and restore canvas state such as
+  // context.lineWidth, context.strokeStyle, and context.fillStyle.
+  // Using them in this way ensures that the canvas is restored to
+  // its original state after the function is done.
+  // This is good practice when writing drawing code.
+  // It avoids side effects and allows the calling code to keep control
+  // of the canvas context.
+  ctx.restore();
+}
