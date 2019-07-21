ee4025568e99cb221c44c0d0a855ed5361315d47
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-21 13:25:33 +0545

    feat(drawing): Draw at current origin
    Create function to draw ship at current origin
    
    Closes #27

--- exercise4/drawing.js
+++ exercise4/drawing.js
@@ -42,6 +42,37 @@ function draw_grid({
   ctx.restore();
 }
 
+function draw_ship_at_origin(ctx, radius, options = {}) {
+  ctx.save();
+  if (options.guide) {
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
+  ctx.lineWidth = options.lineWidth || 2;
+  ctx.strokeStyle = options.stroke || 'white';
+  ctx.fillStyle = options.fill || 'black';
+  let angle = (options.angle || 0.5 * Math.PI) / 2;
+  ctx.beginPath();
+  ctx.moveTo(radius, 0);
+  ctx.lineTo(
+    Math.cos(Math.PI - angle) * radius,
+    Math.sin(Math.PI - angle) * radius
+  );
+  ctx.lineTo(
+    Math.cos(Math.PI + angle) * radius,
+    Math.sin(Math.PI + angle) * radius
+  );
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
+
 function draw_ship({ctx, x, y, radius, options = {}}) {
   ctx.save();
   // optionally draw a guide showing the collision radius
