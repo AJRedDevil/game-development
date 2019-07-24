dad02acc9e021954200546b694d0fef352c4d9fa
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-24 20:15:42 +0545

    feat(animation): Draw the Scene
    A Function to Draw the Scene
    
    #41

--- exercise7/drawing.js
+++ exercise7/drawing.js
@@ -35,17 +35,30 @@ function draw_grid({
   ctx.restore();
 }
 
-function draw_pacman(ctx, radius, mouth) {
-  const angle = 0.2 * Math.PI * mouth;
+function draw_asteroid(ctx, radius, shape, options = {}) {
+  ctx.strokeStyle = options.stroke || 'white';
+  ctx.fillStyle = options.fill || 'black';
   ctx.save();
-  ctx.fillStyle = 'yellow';
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 0.5;
   ctx.beginPath();
-  ctx.arc(0, 0, radius, angle, -angle);
-  ctx.lineTo(0, 0);
+  for (let i = 0; i < shape.length; i++) {
+    ctx.rotate((2 * Math.PI) / shape.length);
+    ctx.lineTo(radius + radius * options.noise * shape[i], 0);
+  }
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
+  if (options.guide) {
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 0.2;
+    ctx.arc(0, 0, radius + radius * noise, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius - radius * noise, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
   ctx.restore();
 }
