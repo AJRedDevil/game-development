4f73ee2864af70e366797f4d05b4e72c984c14d2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-25 20:21:47 +0545

    feat(animation): The PacMan Constructor
    #47

---
+++ exercise8/objects.js
@@ -0,0 +1,11 @@
+function Pacman(x, y, radius, speed) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.speed = speed;
+  this.angle = 0;
+  this.x_speed = speed;
+  this.y_speed = 0;
+  this.time = 0;
+  this.mouth = 0;
+}
