08faffa69b843fec7b6e4df3748652b8352e6ba7
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-27 21:21:24 +0545

    refactor(game): Remove unwanted codes
    #56

--- exercise10/objects.js
+++ exercise10/objects.js
@@ -19,42 +19,7 @@ Pacman.prototype.draw = function(ctx) {
   ctx.restore();
 };
 
-Pacman.prototype.turn = function(direction) {
-  if (this.y_speed) {
-    // if we are travelling vertically
-    // set the horizontal speed and apply the direction
-    this.x_speed = -direction * this.x_speed;
-    // clear the vertical speed and rotate
-    this.y_speed = 0;
-    this.angle = this.x_speed > 0 ? 0 : Math.PI;
-  } else {
-    // if we are travelling horizontally
-    // set the vertical speed and apply the direction
-    this.y_speed = direction * this.x_speed;
-    // clear the horizontal speed and rotate
-    this.x_speed = 0;
-    this.angle = this.y_speed > 0 ? 0.5 * Math.PI : 1.5 * Math.PI;
-  }
-};
-
-Pacman.prototype.turn_left = function() {
-  this.turn(-1);
-};
-
-Pacman.prototype.turn_right = function() {
-  this.turn(1);
-};
-
 Pacman.prototype.update = function(elapsed, width, height) {
-  // an average of once every 100 frames
-  if (Math.random() <= 0.01) {
-    if (Math.random() < 0.5) {
-      this.turn_left();
-    } else {
-      this.turn_right();
-    }
-  }
-
   if (this.x - this.radius + elapsed * this.x_speed > width) {
     this.x = -this.radius;
   }
@@ -93,6 +58,7 @@ Pacman.prototype.move_up = function() {
   this.y_speed = -this.speed;
   this.angle = 1.5 * Math.PI;
 };
+
 // Ghost Class
 function Ghost(x, y, radius, speed, color) {
   this.x = x;
