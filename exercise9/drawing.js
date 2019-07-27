e81065e17d3d20201bd460d9afbe8499f1fe2432
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-27 11:09:02 +0545

    fix(animation): Fix the draw ship funtion
    #54

--- exercise9/drawing.js
+++ exercise9/drawing.js
@@ -133,58 +133,38 @@ function draw_asteroid(ctx, radius, shape, options = {}) {
   ctx.restore();
 }
 
-function draw_ship(
-  ctx,
-  radius,
-  options = {
-    guide: false,
-    lineWidth: 2,
-    stroke: 'white',
-    fill: 'black',
-    curve1: 0.25,
-    curve2: 0.75,
-  }
-) {
+function draw_ship(ctx, radius, options = {}) {
   ctx.save();
-  if (options.guide) {
-    ctx.strokeStyle = 'white';
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-    ctx.lineWidth = 0.5;
-    ctx.beginPath();
-    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-  }
-  ctx.lineWidth = options.lineWidth;
-  ctx.strokeStyle = options.stroke;
-  ctx.fillStyle = options.fill;
+  ctx.lineWidth = options.lineWidth || 2;
+  ctx.strokeStyle = options.stroke || 'white';
+  ctx.fillStyle = options.fill || 'black';
   let angle = (options.angle || 0.5 * Math.PI) / 2;
+  let curve1 = options.curve1 || 0.25;
+  let curve2 = options.curve2 || 0.75;
   ctx.beginPath();
   ctx.moveTo(radius, 0);
-  // here we have three curves
   ctx.quadraticCurveTo(
-    Math.cos(angle) * radius * options.curve2,
-    Math.sin(angle) * radius * options.curve2,
+    Math.cos(angle) * radius * curve2,
+    Math.sin(angle) * radius * curve2,
     Math.cos(Math.PI - angle) * radius,
     Math.sin(Math.PI - angle) * radius
   );
   ctx.quadraticCurveTo(
-    -radius * options.curve1,
+    -radius * curve1,
     0,
     Math.cos(Math.PI + angle) * radius,
     Math.sin(Math.PI + angle) * radius
   );
   ctx.quadraticCurveTo(
-    Math.cos(-angle) * radius * options.curve2,
-    Math.sin(-angle) * radius * options.curve2,
+    Math.cos(-angle) * radius * curve2,
+    Math.sin(-angle) * radius * curve2,
     radius,
     0
   );
+
   ctx.fill();
   ctx.stroke();
-  // the guide drawing code is getting complicated
   if (options.guide) {
-    ctx.strokeStyle = 'white';
     ctx.fillStyle = 'white';
     ctx.lineWidth = 0.5;
     ctx.beginPath();
@@ -196,8 +176,8 @@ function draw_ship(
     ctx.stroke();
     ctx.beginPath();
     ctx.arc(
-      Math.cos(angle) * radius * options.curve2,
-      Math.sin(angle) * radius * options.curve2,
+      Math.cos(angle) * radius * curve2,
+      Math.sin(angle) * radius * curve2,
       radius / 40,
       0,
       2 * Math.PI
@@ -205,16 +185,19 @@ function draw_ship(
     ctx.fill();
     ctx.beginPath();
     ctx.arc(
-      Math.cos(-angle) * radius * options.curve2,
-      Math.sin(-angle) * radius * options.curve2,
+      Math.cos(-angle) * radius * curve2,
+      Math.sin(-angle) * radius * curve2,
       radius / 40,
       0,
       2 * Math.PI
     );
     ctx.fill();
     ctx.beginPath();
-    ctx.arc(radius * options.curve1 - radius, 0, radius / 50, 0, 2 * Math.PI);
+    ctx.arc(-radius * curve1, 0, radius / 40, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
   }
   ctx.restore();
 }
