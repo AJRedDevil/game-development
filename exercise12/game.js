e9e081afe09fed6e30bdca335fda2ca411d2a475
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-28 14:13:08 +0545

    refactor(game): Helper Methods for Creating Asteroids
    The AsteroidsGame.prototype.new_asteroid method simply creates an asteroid in a random position with the asteroid_mass property
    The AsteroidsGame.prototype.push_asteroid helper method pushes and twists an asteroid using the asteroid_push property as the force
    It pushes over the provided elapsed time
    If no elapsed time is provided, it assumes a standard frame at 60 fps (frames per second)
    
    #62

--- exercise12/game.js
+++ exercise12/game.js
@@ -8,16 +8,36 @@ const AsteroidsGame = function(id) {
   this.asteroid_mass = 500;
   this.asteroid_push = 500000; // max force to apply in one frame
 
-	this.ship; = new Ship(
-		this.canvas.width / 2,
-		this.canvas.height / 2,
-		1000,
-		200
-	);
-	this.projectiles = [];
-	this.asteroids = [];
-	this.asteroids.push(this.moving_asteroid());
-	this.canvas.addEventListener("keydown", this.keyDown.bind(this), true);
-	this.canvas.addEventListener("keyup", this.keyUp.bind(this), true);
-	window.requestAnimationFrame(this.frame.bind(this));
+  this.ship = new Ship(
+    this.canvas.width / 2,
+    this.canvas.height / 2,
+    1000,
+    200
+  );
+  this.projectiles = [];
+  this.asteroids = [];
+  this.asteroids.push(this.moving_asteroid());
+  this.canvas.addEventListener('keydown', this.keyDown.bind(this), true);
+  this.canvas.addEventListener('keyup', this.keyUp.bind(this), true);
+  window.requestAnimationFrame(this.frame.bind(this));
+};
+
+AsteroidsGame.prototype.moving_asteroid = function(elapsed) {
+  const asteroid = this.new_asteroid();
+  this.push_asteroid(asteroid, elapsed);
+  return asteroid;
+};
+AsteroidsGame.prototype.new_asteroid = function() {
+  return new Asteroid(
+    this.asteroid_mass,
+    this.canvas.width * Math.random(),
+    this.canvas.height * Math.random()
+  );
+};
+AsteroidsGame.prototype.push_asteroid = function(asteroid, elapsed = 0.015) {
+  asteroid.push(2 * Math.PI * Math.random(), this.asteroid_push, elapsed);
+  asteroid.twist(
+    (Math.random() - 0.5) * Math.PI * this.asteroid_push * 0.02,
+    elapsed
+  );
 };
